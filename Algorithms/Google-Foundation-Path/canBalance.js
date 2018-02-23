81813cfd48043edf4528501b3920c3fa2a7ceece
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-02-23 14:54:44 -0500

    Create canBalance.js

---
+++ Algorithms/Google-Foundation-Path/canBalance.js
@@ -0,0 +1,12 @@
+// Given a non-empty array, return true if there is a place to split the array 
+// so that the sum of the numbers on one side is equal to the sum of the numbers on the other side.
+
+const canBalance = arr => arr.reduce((a,b) => a+b) % 2 === 0;
+
+
+canBalance([1, 1, 1, 2, 1]);
+// → true
+canBalance([2, 1, 1, 2, 1]);
+// → false
+canBalance([10, 10]);
+// → true
