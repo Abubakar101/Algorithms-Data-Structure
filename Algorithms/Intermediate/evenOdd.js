594157ffc1f0f47fc1c0f23ec923e26241187dfe
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-03-22 11:17:05 -0400

    Create evenOdd.js

---
+++ Algorithms/Intermediate/evenOdd.js
@@ -0,0 +1,15 @@
+// Given an array of ints, return the string "even" if there are more even elements in the array, or "odd" if there are more odd elements in the array.
+function evenOdd(arr) {
+	let even = 0,
+		odd = 0;
+
+	for (let num of arr) {
+		if (num % 2 === 0) {
+			event++;
+		} else {
+			odd++;
+		}
+	}
+	return even > odd ? `even` : `odd`;
+}
+console.log('Even-OR-Odd => ', evenOdd([2, 4, 6, 3, 5, 7, 9]));
