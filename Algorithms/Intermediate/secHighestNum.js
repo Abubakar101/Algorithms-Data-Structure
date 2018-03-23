28660943d7192f9af15c98c5eb37af9a5590ee20
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-03-22 23:21:59 -0400

    Update secHighestNum.js

--- Algorithms/Intermediate/secHighestNum.js
+++ Algorithms/Intermediate/secHighestNum.js
@@ -1,3 +1,20 @@
 // Write a method that finds the second highest number in an array of integers.
-const secHighestNum = arr => arr.sort((a, b) => b - a)[1];
+
+
+// const secHighestNum = arr => arr.sort((a, b) => b - a)[1];
+
+
+function secHighestNum(arr) {
+	let largestNum = 0,
+		secLargestNum = 0;
+
+	for (let num of arr) {
+		if (num > largestNum) {
+			largestNum = num;
+		} else if (num > secLargestNum && num < largestNum) {
+			secLargestNum = num;
+		}
+	}
+	return secLargestNum;
+}
 console.log('SecHighestNum => ', secHighestNum([1, 5, 10, 50, 15]));
