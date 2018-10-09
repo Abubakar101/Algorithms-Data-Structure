947624905a74c4689764f157f07fae55e87bda95
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-10-09 16:52:21 -0400

    Create strRepeat.js

---
+++ Algorithms/Easy/strRepeat.js
@@ -0,0 +1,7 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+const repeatStr = (n,s) => s.repeat(n);
+
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
