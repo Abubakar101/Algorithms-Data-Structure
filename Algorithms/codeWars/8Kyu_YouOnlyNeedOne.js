b5c47623b193a7bd5cfdf4e80eb4ae40e367fd44
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2018-11-04 12:53:35 -0500

    Create 8Kyu_YouOnlyNeedOne.js

---
+++ Algorithms/codeWars/8Kyu_YouOnlyNeedOne.js
@@ -0,0 +1,8 @@
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not.
+
+const check = (a,x) => a.includes(x);
+
+numberToString(123); // returns '123';`   
+numberToString(999); // returns '999';`
